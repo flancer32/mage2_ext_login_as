337ca22a6c6e4f6ca963fe869421f952226cef3e
Alex Gusev <alex@flancer64.com>
2017-04-05 20:24:13 +0300

    Registry redirect activity

--- src/Controller/Adminhtml/Redirect/Index.php
+++ src/Controller/Adminhtml/Redirect/Index.php
@@ -22,13 +22,33 @@ class Index
 
     public function execute()
     {
+        /* collect redirection parameters and registry activity */
         $custId = $this->_request->getParam(self::REQ_PARAM_ID);
         $user = $this->_auth->getUser();
         $userId = $user->getUserId();
+        $key = $this->generateKey($custId, $userId);
         $entity = new \Flancer32\LoginAs\Repo\Data\Entity\Active();
         $entity->setCustomerRef($custId);
         $entity->setUserRef($userId);
+        $entity->setKey($key);
         $actId = $this->repoActive->create($entity);
-        $saved = $this->repoActive->getById($actId);
+        $this->repoActive->getById($actId);
+        /* redirect to frontend using search key for the activity */
+
+    }
+
+    /**
+     * Generate unique key to get redirection data as customer.
+     *
+     * @param $custId
+     * @param $userId
+     * @return string
+     */
+    protected function generateKey($custId, $userId)
+    {
+        $now = date('YmdHis');
+        $source = "cust:$custId;user:$userId;at:$now";
+        $result = $now . md5($source);
+        return $result;
     }
 }
