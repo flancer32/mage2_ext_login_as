54bdd2fe65350b4d789f04f6d462b7205202e66b
Alex Gusev <alex@flancer64.com>
2017-04-07 12:34:16 +0300

    Logs

--- src/Controller/Redirect/Index.php
+++ src/Controller/Redirect/Index.php
@@ -15,6 +15,8 @@ class Index
     protected $repoActive;
     /** @var \Magento\Customer\Api\CustomerRepositoryInterface */
     protected $repoCust;
+    /** @var \Flancer32\LoginAs\Repo\Entity\ILog */
+    protected $repoLog;
     /** @var \Magento\Customer\Model\Session */
     protected $session;
 
@@ -23,12 +25,14 @@ class Index
         \Magento\Framework\Controller\Result\RedirectFactory $factRedirect,
         \Magento\Customer\Api\CustomerRepositoryInterface $repoCust,
         \Flancer32\LoginAs\Repo\Entity\IActive $repoActive,
+        \Flancer32\LoginAs\Repo\Entity\ILog $repoLog,
         \Magento\Customer\Model\Session $session
     ) {
         parent::__construct($context);
         $this->factRedirect = $factRedirect;
         $this->repoCust = $repoCust;
         $this->repoActive = $repoActive;
+        $this->repoLog = $repoLog;
         $this->session = $session;
     }
 
@@ -39,12 +43,21 @@ class Index
         /* get active redirection and extract customer attributes */
         $entity = $this->repoActive->getById($key);
         $custId = $entity->getCustomerRef();
+        $userId = $entity->getUserRef();
         /* load customer and initiate session */
         $customer = $this->repoCust->getById($custId);
         $this->session->setCustomerDataAsLoggedIn($customer);
         $this->session->regenerateId();
         /* remove used redirection from active registry */
         $this->repoActive->deleteById($key);
+        /* log 'login as' event */
+        $date = gmdate('Y-m-d H:i:s');
+        $local = date('Y-m-d H:i:s');
+        $log = new \Flancer32\LoginAs\Repo\Data\Entity\Log();
+        $log->setCustomerRef($custId);
+        $log->setUserRef($userId);
+        $log->setDate($date);
+        $this->repoLog->create($log);
         /* redirect authenticated customer to the homepage */
         $resultRedirect = $this->factRedirect->create();
         $resultRedirect->setPath('customer/account');
