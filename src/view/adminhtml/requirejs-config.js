2e6153fccc8e6961da37180665cb56d4b64c2024
Alex Gusev <alex@flancer64.com>
2017-04-30 13:10:57 +0300

    Improve grid for logs

---
+++ src/view/adminhtml/requirejs-config.js
@@ -0,0 +1,8 @@
+// https://magento.stackexchange.com/questions/172366/override-and-replace-default-js-component
+// var config = {
+//     map: {
+//         '*': {
+//             'Magento_Ui/js/grid/columns/actions': 'Flancer32_LoginAs/ui/js/grid/columns/actions'
+//         }
+//     }
+// };
---
+++ src/view/adminhtml/web/ui/js/grid/columns/actions.js
@@ -0,0 +1,13 @@
+/**
+ * Add target="_blank" for LoginAs actions in grid.
+ */
+define([
+    "jquery",
+    "Magento_Ui/js/grid/columns/actions"
+], function ($, Component) {
+    "use strict"
+
+    debugger
+    /* https://magento.stackexchange.com/questions/172366/override-and-replace-default-js-component */
+    return Component
+})
